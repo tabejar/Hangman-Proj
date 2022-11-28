301dd0245262305544f10dc1da2f7f60e0be07e9
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-11-28 18:57:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+list_of_words = [
+  "fortnite", "goku", "jonesy", "fishsticks", "heisenberg", "saul",
+  "meowscles", "midas", "vegeta", "beerus", "bulma"
+]
+word = random.choice(list_of_words)
+print(
+  "Welcome to hangman where you have to guess the word but if u get it wrong 6 times u die to tyler ninya blevns gioode luick"
+)
+print(word)
---
+++ tries.py
@@ -0,0 +1,1 @@
+def game(word):
